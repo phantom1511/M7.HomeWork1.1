b12827f2df60d9913130e304a6c50d836c63d7ad
phantom1511 <dastan.tulokulov@gmail.com>
2020-01-22 18:03:59 +0600

    Добавил View

--- app/src/main/java/com/dastan/m7homework11/MainActivity.java
+++ app/src/main/java/com/dastan/m7homework11/MainActivity.java
@@ -1,8 +1,14 @@
 package com.dastan.m7homework11;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.bottomNavView);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
